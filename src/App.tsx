056d2c2ed06635e6e2a7020b4de1bd400ffe06d1
Егор <saleroses@yandex.ru>
2023-03-11 23:43:18 +0300

    messages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
+    let Dialog;
     return (
         <BrowserRouter>
             <div className={"app-wrapper"}>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import s from "/Dialogs.module.css"
+import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrei
+                </div>
+                <div className={s.dialog}>
+                    Petya
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>Hiii</div>
+                <div className={s.message}>Kyky</div>
+                <div className={s.message}>4e kogo</div>
+                <div className={s.message}>Go home?</div>
+                <div className={s.message}>How are you?</div>
+            </div>
         </div>
+
     );
 };
 
