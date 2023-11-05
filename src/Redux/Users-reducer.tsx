ed652537cb4773a03598880dee4538def5ae0361
Егор <saleroses@yandex.ru>
2023-11-05 14:51:46 +0300

    add users page

--- src/Redux/Users-reducer.tsx
+++ src/Redux/Users-reducer.tsx
@@ -1,58 +1,39 @@
 import {v1} from "uuid";
+import {AddPostAT, LikePostAT, UpdateNewPostTextAT} from "./Profile-reducer";
 
-export type PostsDataType = {
+export type UserDataType = {
     id: string
-    message: string
-    likeCounter: number
+    followed: boolean
+    fullName: string
+    status: string
+    location: {
+        city: string,
+        country: string,
+    }
+
 }
 
-export type ProfilePageType = {
-    newPostText: string
-    postsData: Array<PostsDataType>
-
+export type UsersPageType = {
+    users: Array<UserDataType>
 }
 
-export type AddPostAT = ReturnType<typeof AddPostAC>
-export type UpdateNewPostTextAT = ReturnType<typeof UpdateNewPostTextAC>
-export type LikePostAT = ReturnType<typeof LikePostAC>
-export type ActionType = AddPostAT | UpdateNewPostTextAT | LikePostAT
+export type FollowAT = ReturnType<typeof FollowAC>
+export type UnFollowAT = ReturnType<typeof UnFollowAC>
+
+export type ActionType = FollowAT | UnFollowAT
 
 
 
-let initialState: ProfilePageType = {
-    newPostText: '',
-    postsData: [
-        {id: v1(), message: "How are you?", likeCounter: 12},
-        {id: v1(), message: "First post", likeCounter: 32},
-        {id: v1(), message: "First first post)", likeCounter: 16},
+let initialState: UsersPageType = {
+    users: [
+        {id: v1(), followed: false, fullName: "name1", status: "I'm a boss", location: {city: 'Minsk', country: 'Belarus'}},
+        {id: v1(), followed: true, fullName: "name2", status: "status", location: {city: 'SPb', country: 'Russia'}},
+        {id: v1(), followed: true, fullName: "name3", status: "i'm fine", location: {city: 'Moscow', country: 'Russia'}},
     ]}
 
-export const ProfileReducer = (state = initialState, action: ActionType) => {
+export const usersReducer = (state = initialState, action: ActionType) => {
 
     switch (action.type) {
-        case "ADD-POST":
-            let newPost: PostsDataType = {
-                id: v1(),
-                message: state.newPostText,
-                likeCounter: 0,
-            }
-            return {...state,
-                newPostText: '',
-                postsData: [newPost, ...state.postsData],
-            }
-
-        case "UPDATE_NEW_POST_TEXT":
-            return {...state,
-                postsData: [...state.postsData],
-                newPostText: action.text
-            }
-
-        case "LIKE-POST":
-
-            return {...state,
-                postsData: [...state.postsData]
-                    .map( (p)=> p.id === action.postId ? {...p, likeCounter: p.likeCounter+1} : p)
-            }
 
         default:
             return state
@@ -60,34 +41,14 @@ export const ProfileReducer = (state = initialState, action: ActionType) => {
 }
 
 
-export const AddPostAC = () => {
-    return {type: "ADD-POST"} as const
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+
+export const FollowAC = () => {
+    return {type: FOLLOW} as const
 }
 
-export const UpdateNewPostTextAC = (text: string) => {
-    return {type: "UPDATE_NEW_POST_TEXT", text} as const
+export const UnFollowAC = () => {
+    return {type: UNFOLLOW} as const
 }
 
-export const LikePostAC = (postId: string) => {
-    return {type: "LIKE-POST", postId} as const
-}
-
-
-
-
-
-// switch (action.type) {
-//     case ADD_POST:
-//         let newPost: PostsDataType = {
-//             id: v1(),
-//             message: action.postMessage,
-//             likeCounter: 0
-//         }
-//         state.postsData.unshift(newPost);
-//         state.newPostText = ''
-//         return state;
-//
-//     case UPDATE_NEW_POST_TEXT:
-//         state.newPostText = action.newPostText
-//         return state;
-// }
